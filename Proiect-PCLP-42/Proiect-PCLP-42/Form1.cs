5bf145fc0c35a529c8d48f49c2ff2ce1ad0756f7
MihaiMocanu18 <mihai.mocanu89@icloud.com>
2022-11-18 15:02:48 +0200

    programare buton convert

--- Proiect-PCLP-42/Proiect-PCLP-42/Form1.cs
+++ Proiect-PCLP-42/Proiect-PCLP-42/Form1.cs
@@ -16,5 +16,14 @@ namespace Proiect_PCLP_42
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int label2 = int.Parse(textBox1.Text);
+            label7.Text = Convert.ToString(label2, 2);
+            label8.Text = label2.ToString();
+            label9.Text = Convert.ToString(label2, 16);
+            label10.Text = Convert.ToString(label2, 8);
+        }
     }
 }
